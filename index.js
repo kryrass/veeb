5a9feda051112bcffb8db9f421587fbe92171d6e
Kryslin Rass <kryrass@greeny.cs.tlu.ee>
2023-10-19 17:25:07 +0300

    Viimane tund enne vahenädalat

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const express = require('express');
-const timeInfo = require('./dateTimeFnc');
 const fs = require("fs");
 const app = express();
+const mysql = require('mysql2');
+const timeInfo = require('./dateTimeFnc');
+const bodyparser = require('body-parser');
+const dbInfo = require('../../vp23config.js');
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+app.use(bodyparser.urlencoded({extended: false}));
+
+//loon andmbebaasi ühenduse
+const conn = mysql.createConnection({
+	host: dbInfo.configData.host,
+	user: dbInfo.configData.user,
+	password: dbInfo.configData.password,
+	database: dbInfo.configData.database
+});
 
 app.get('/', (req, res)=>{
 	//res.send('See töötab!');
@@ -39,11 +51,78 @@ fs.readFile('public/txtfiles/log.txt', 'utf8', (err, data)=>{
 		throw err;
 	}
 		else {
-			namelist = data.replaceALL(",", " ").split(';');
+			namelist = data.replaceAll(",", " ").split(';');
 			res.render('namelist', {h1: 'nimed', name: namelist});
 		}
 	});
 
 });
+
+app.get('/eestifilm', (req, res)=>{
+	res.render('filmindex');
+});
+
+app.get('/eestifilm/filmiloend', (req, res)=>{
+	let sql = 'SELECT title, production_year, duration FROM movie';
+	let sqlResult = [];
+	conn.query(sql, (err, result)=>{
+		if (err){
+			res.render('filmlist', {filmlist: sqlResult});
+			throw err;
+			//conn.end();
+		}
+		else {
+			//console.log(result);
+			res.render('filmlist', {filmlist: result});
+			//conn.end();
+		}
+	});
+	
+});
+
+app.get('/eestifilm/addfilmperson', (req, res)=>{
+	res.render('addfilmperson');
+});
+
+app.post('/eestifilm/addfilmperson', (req, res)=>{
+	//res.render('filmindex');
+	//res.send("req.body");
+	let notice = '';
+	let sql = 'INSERT INTO person (first_name, last_name, birth_date) VALUES(?,?,?)';
+	conn.query(sql, [req.body.firstNameInput, req.body.lastNameInput, req.body.birthDateInput], (err, result)=>{
+		if (err) {
+			notice = 'Andmete salvestamine ebaõnnestus!';
+			res.render('addfilmperson', {notice: notice});
+			throw err;
+		}
+		else {
+			notice = req.body.firstNameInput + ' ' + req.body.lastNameInput + ' salvestamine õnnestus!';
+			res.render('addfilmperson', {notice: notice}); 
+
+		}
+	});
+});
+
+app.get('/eestifilm/singlemovie', (req, res)=>{
+	res.render('singlemovie');
+});
+
+app.post('/eestifilm/singlemovie', (req, res)=>{
+let notice = '';
+	let sql = 'SELECT COUNT(id) FROM movie';
+	conn.query(sql, [req.body.firstNameInput, req.body.lastNameInput, req.body.birthDateInput], (err, result)=>{
+		if (err) {
+			notice = 'Andmete salvestamine ebaõnnestus!';
+			res.render('singlemovie', {notice: notice});
+			throw err;
+		}
+		else {
+			//notice = req.body.firstNameInput + ' ' + req.body.lastNameInput + ' salvestamine õnnestus!';
+			res.render('singlemovie', {notice: notice}); 
+
+		}
+	});
+});
+
 
 app.listen(5109);
